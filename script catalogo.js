cbe6acabdbf4fc5c29406bc075aa78622d0c1349
López <mg.mascarillas2021@gmail.com>
2021-06-03 03:01:51 -0600

    mi primer commit

---
+++ script catalogo.js
@@ -0,0 +1,12 @@
+function abrir(){
+	//document.getElementById("negra").style.display="block";
+	alert('► precio: $3.75 \n \n ► Descripción: Caja de mascarillas de color negro elegante que tienen un porcentaje alto de eficiencia al filtrar el aire con un porcentaje de filtración ≥ 95%.');
+}
+function abrir2(){
+	//document.getElementById("negra").style.display="block";
+	alert('► precio: $1.50 \n \n ► Descripción: Caja de mascarillas tradicionales de cálidad que tienen un porcentaje alto de eficiencia al filtrar el aire con un porcentaje de filtración ≥ 98%.');
+}
+function abrir3(){
+	//document.getElementById("negra").style.display="block";
+	alert('► precio: $2.50 \n \n ► Descripción: Caja de mascarillas de colores que tienen un porcentaje alto de eficiencia al filtrar el aire con un porcentaje de filtración ≥ 95%.');
+}
